70e3bd95ea9014d077467310344d8cf32a2afde1
Achmad96 <totsukamail3278@gmail.com>
2024-08-20 15:38:55 +0700

    fix: blog's bugs

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -3,6 +3,8 @@ import { Suspense } from 'react';
 
 import Blogs from '@/components/ui/blogs';
 
+export const revalidate = 600; // revalidate the data every 10 minutes
+
 interface IPage {
   searchParams: {
     startCursor: string;
@@ -11,7 +13,7 @@ interface IPage {
 
 export default function Page({ searchParams }: IPage) {
   return (
-    <section className="flex h-auto min-h-[90dvh] w-full flex-col items-center justify-center gap-7">
+    <section className="flex h-[90dvh] w-full flex-col items-center justify-center gap-7">
       <Suspense fallback={<BlogsSkeleton />}>
         <Blogs searchParams={searchParams} />
       </Suspense>
